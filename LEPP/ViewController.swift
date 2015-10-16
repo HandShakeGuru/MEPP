370794002c0fde6845196da73f341546d297df85
Garet McKinley <garetmckinley@me.com>
2015-10-15 22:36:12 -0400

    Initial Commit

---
+++ LEPP/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  LEPP
+//
+//  Created by Garet McKinley on 10/15/15.
+//  Copyright © 2015 Garet McKinley. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ LEPPTests/LEPPTests.swift
@@ -0,0 +1,36 @@
+//
+//  LEPPTests.swift
+//  LEPPTests
+//
+//  Created by Garet McKinley on 10/15/15.
+//  Copyright © 2015 Garet McKinley. All rights reserved.
+//
+
+import XCTest
+@testable import LEPP
+
+class LEPPTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
